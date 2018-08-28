bfa894e44e3d3b0695340a600712b34d086a89a2
Devin Flood <devin.p.flood@gmail.com>
2018-08-28 08:31:27 -0700

    Finish Mancala Board class

--- W2D2/skeleton/lib/board.rb
+++ W2D2/skeleton/lib/board.rb
@@ -2,6 +2,7 @@ class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
+    @name1, @name2 = name1, name2
     @cups = Array.new(14) { [] }
     place_stones
   end
@@ -16,13 +17,45 @@ class Board
   end
 
   def valid_move?(start_pos)
+    # valid_starts = [0, 1, 2, 3, 4, 5, 7, 8, 9, 10, 11, 12]
+    valid_starts = (0..5).to_a + (7..12).to_a
+    raise "Invalid starting cup" unless valid_starts.include?(start_pos)
+    raise "Starting cup is empty" if @cups[start_pos].empty?
+    true
   end
 
   def make_move(start_pos, current_player_name)
+    if valid_move?(start_pos)
+      in_hand = @cups[start_pos].dup
+      @cups[start_pos] = []
+
+      cup_idx = start_pos
+      until in_hand.empty?
+        cup_idx += 1
+        cup_idx = 0 if cup_idx > 13
+        if cup_idx == 6
+          @cups[6] << in_hand.pop if current_player_name == @name1
+        elsif cup_idx == 13
+          @cups[13] << in_hand.pop if current_player_name == @name2
+        else
+          @cups[cup_idx] << in_hand.pop
+        end
+      end
+    end
+
+    render
+    next_turn(cup_idx)
   end
 
   def next_turn(ending_cup_idx)
     # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
+    if ending_cup_idx == 6 || ending_cup_idx == 13
+      :prompt
+    elsif @cups[ending_cup_idx].length == 1
+      :switch
+    else
+      ending_cup_idx
+    end
   end
 
   def render
@@ -34,8 +67,17 @@ class Board
   end
 
   def one_side_empty?
+    @cups[0..5].all? {|cup| cup.empty?} || @cups[7..12].all?  {|cup| cup.empty?}
   end
 
   def winner
+    player1_points = @cups[6].length
+    player2_points = @cups[13].length
+
+    if player1_points == player2_points
+      :draw
+    else
+      player1_points > player2_points ? @name1 : @name2
+    end
   end
 end
