bfe9a95f0ffaabaf3f7060f3144caadaabe00f90
Devin Flood <devin.p.flood@gmail.com>
2018-08-27 21:09:34 -0700

    Add #initialize & #place_stones methods

--- W2D2/skeleton/lib/board.rb
+++ W2D2/skeleton/lib/board.rb
@@ -3,10 +3,16 @@ class Board
 
   def initialize(name1, name2)
     @cups = Array.new(14) { [] }
+    place_stones
   end
 
   def place_stones
     # helper method to #initialize every non-store cup with four stones each
+    @cups.each_with_index do |cup, idx|
+      unless idx == 6 || idx == 13
+        4.times { cup << :stone }
+      end
+    end
   end
 
   def valid_move?(start_pos)
