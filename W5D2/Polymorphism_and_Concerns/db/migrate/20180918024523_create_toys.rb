5c073e6f399298803aa660c203067063b3dc2f67
Devin Flood <devin.p.flood@gmail.com>
2018-09-17 19:51:27 -0700

    Finish W5D2 homework

---
+++ W5D2/Polymorphism_and_Concerns/db/migrate/20180918024523_create_toys.rb
@@ -0,0 +1,12 @@
+class CreateToys < ActiveRecord::Migration[5.1]
+  def change
+    create_table :toys do |t|
+      t.string :name, null: false
+      t.integer :toyable_id, polymorphic: true, index: true
+      t.string :toyable_type
+
+      t.timestamps
+    end
+    add_index :toys, [:name, :toyable_id, :toyable_type], unique: true
+  end
+end
